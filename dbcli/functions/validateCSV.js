be978332003ef095c01bbe9bda94a59a8599b4ba
ebbmango <ebbmango@tuta.io>
2024-05-14 07:14:48 +0200

    now path validation accepts only files inside import folder

--- dbcli/functions/validateCSV.js
+++ dbcli/functions/validateCSV.js
@@ -1,6 +1,10 @@
+const path = require("path");
 const fs = require("fs");
 
 function validateCSV(argv) {
+  // Saving the path to the dedicated import folder
+  const importDir = path.resolve(__dirname, "../import");
+
   // Stows only the arguments (command name excluded) in an array.
   const arguments = argv._.slice(1);
 
@@ -9,15 +13,20 @@ function validateCSV(argv) {
     throw new Error("Error: You must provide the pathname of the CSV file.");
   }
 
-  // Retrieves the file path.
+  // Retrieves the file path informed by the user from the arguments array.
   const filePath = arguments[0];
 
+  // Resolves the pathname given by the user inside of the import folder.
+  const importPath = `${importDir}/${filePath}`;
+
   // Checks whether the file exists...
-  if (!fs.existsSync(filePath)) {
-    throw new Error(`File ${filePath} does not exist.`);
+  if (!fs.existsSync(importPath)) {
+    // If it doesn't, alert the user and halts the program.
+    throw new Error(`File ${importPath} does not exist.`);
   }
 
-  return filePath;
+  // If it does, return the path to it.
+  return importPath;
 }
 
 module.exports = { validateCSV };
