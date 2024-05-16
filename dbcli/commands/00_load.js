fd27aac830e173f4619b3a76b797a89c976c32ee
ebbmango <ebbmango@tuta.io>
2024-05-16 12:28:10 +0200

    code cleanup

--- dbcli/commands/00_load.js
+++ dbcli/commands/00_load.js
@@ -14,8 +14,9 @@ const command = {
   describe:
     "Loads the database with the data provided by the CSV file passed as its argument.",
   handler: async (argv) => {
-    // 01 - Validating the file
+
     let filePath;
+
     try {
       filePath = validateCSV(argv); // Checks whether the user has provided a valid file path
       console.log("file has been found in the directory");
@@ -30,45 +31,3 @@ const command = {
 };
 
 module.exports = command;
-
-// function prettifyRow(row) {
-//   return row.map((string) => {
-//     return string.replace(/_/g, " ");
-//   });
-// }
-
-// 03 - Reading the whole CSV file:
-// const batchSize = 10000;
-// let batches = 0;
-// let rowCount = 0;
-// let batch = [];
-
-// fs.createReadStream(filePath)
-//   .pipe(
-//     parse({
-//       delimiter: ",",
-//       escape: "\\",
-//     })
-//   )
-//   .on("data", async (row) => {
-//     const prettyRow = prettifyRow(row);
-//     batch.push(prettyRow);
-//     rowCount++;
-
-//     // if the max size for a batch has been reached OR if all rows have been read
-//     if (rowCount % batchSize === 0 || rowCount === totalRows) {
-//       try {
-//         batches++;
-//         const filePath = await writeBatchToCSV(batch);
-//         console.log(`Batch no. ${batches} written to file.`);
-//         await uploadBatch(filePath);
-//       } catch (error) {
-//         console.error("Error writing CSV file:", error);
-//       }
-
-//       batch = [];
-//     }
-//   })
-//   .on("end", () => {
-//     console.log(batches.length + " batches have been created.");
-//   });
