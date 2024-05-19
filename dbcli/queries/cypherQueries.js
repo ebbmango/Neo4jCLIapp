be881979460dd0e30a99b2e690bb83208c2f586f
ebbmango <ebbmango@tuta.io>
2024-05-19 18:03:51 +0200

    added queries for task no. 9

--- dbcli/queries/cypherQueries.js
+++ dbcli/queries/cypherQueries.js
@@ -1,37 +1,37 @@
-// 01 - This query FINDS ALL CHILDREN of the node whose name is given by the `nodeName` parameter
+// 01 - This query FINDS ALL CHILDREN of the node whose name is given by the "nodeName" parameter
 const findChildrenQuery = ` 
 MATCH (node:Category {name: $categoryName})-[:HAS_SUBCATEGORY]->(child:Category) 
 RETURN child 
 ORDER BY child.name ASC 
 `;
 
-// 02 - This query COUNTS ALL CHILDREN of the node whose name is given by the `nodeName` parameter
+// 02 - This query COUNTS ALL CHILDREN of the node whose name is given by the "nodeName" parameter
 const countChildrenQuery = ` 
 MATCH (node:Category {name: $categoryName})-[:HAS_SUBCATEGORY]->(child:Category) 
 RETURN COUNT (child) AS childrenCount 
 `;
 
-// 03 - This query FINDS ALL GRANDCHILDREN of the node whose name is given by the `nodeName` parameter
+// 03 - This query FINDS ALL GRANDCHILDREN of the node whose name is given by the "nodeName" parameter
 const findGrandchildrenQuery = `
 MATCH (node:Category {name: $categoryName})-[:HAS_SUBCATEGORY*2]->(grandchild:Category)
 RETURN grandchild
 ORDER BY grandchild.name ASC
 `;
 
-// 04 - This query FINDS ALL PARENTS of the node whose name is given by the `nodeName` parameter
+// 04 - This query FINDS ALL PARENTS of the node whose name is given by the "nodeName" parameter
 const findParentsQuery = ` 
 MATCH (node:Category {name: $categoryName})<-[:HAS_SUBCATEGORY]-(parent:Category) 
 RETURN parent 
 ORDER BY parent.name ASC 
 `;
 
-// 05 - This query COUNTS ALL PARENTS of the node whose name is given by the `nodeName` parameter
+// 05 - This query COUNTS ALL PARENTS of the node whose name is given by the "nodeName" parameter
 const countParentsQuery = ` 
 MATCH (node:Category {name: $categoryName})<-[:HAS_SUBCATEGORY]-(parent:Category) 
 RETURN COUNT (parent) AS parentsCount 
 `;
 
-// 06 - This query FINDS ALL GRANDPARENTS of the node whose name is given by the `nodeName` parameter
+// 06 - This query FINDS ALL GRANDPARENTS of the node whose name is given by the "nodeName" parameter
 const findGrandparentsQuery = ` 
 MATCH (node:Category {name: $categoryName})<-[:HAS_SUBCATEGORY*2]-(grandparent:Category) 
 RETURN grandparent 
@@ -41,13 +41,29 @@ ORDER BY grandparent.name ASC
 // 07 - This query COUNTS ALL UNIQUELY NAMED NODES.
 const countUniqueNodesQuery = `MATCH (node:Category) RETURN COUNT (DISTINCT node.name) AS uniqueCount`;
 
-// 08 - This query RETRIEVES A RANDOM ROOT NODE
+// 08 - This query FINDS RANDOM ROOT NODES in the amount given by the "amount" parameter.
 const findRandomRoot = `
 MATCH (root)-[:HAS_SUBCATEGORY]->()
 WHERE NOT exists( ()-[:HAS_SUBCATEGORY]->(root) )
 RETURN DISTINCT root.name, rand() AS r 
 ORDER BY r
 LIMIT $amount
+`
+
+// 09a - This query finds out what is THE HIGHEST AMOUNT OF CHILDREN that a node has.
+const findMaxChildrenCountQuery = `
+MATCH (node:Category)-[:HAS_SUBCATEGORY]->(child:Category)
+WITH node, COUNT(child) AS childrenCount
+WITH MAX(childrenCount) AS maxChildrenCount
+RETURN maxChildrenCount;
+`
+
+// 09b - This query FINDS ALL NODES whose AMOUNT OF CHILDREN is given by the "amount" parameter.
+const findFertileQuery = `
+MATCH (node:Category)-[:HAS_SUBCATEGORY]->(child:Category)
+WITH node, COUNT(child) AS childCount
+WHERE childCount = $amount
+RETURN node, childCount
 `
 
 module.exports = {
