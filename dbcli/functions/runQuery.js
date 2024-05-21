eb2d41d1d9d38a8696859d3bf4b9a3b4b6033e19
ebbmango <ebbmango@tuta.io>
2024-05-21 11:20:09 +0200

    returns execution time for each executed query

--- dbcli/functions/runQuery.js
+++ dbcli/functions/runQuery.js
@@ -5,15 +5,19 @@ const runQuery = async (query, queryParameters) => {
   const driver = neo4j.driver("bolt://localhost:7687");
   const session = driver.session();
 
+  var startTime = performance.now();
+
   // Runs the query.
   const result = await session.run(query, queryParameters);
+
+  var endTime = performance.now();
 
   // Terminates the queries' runner.
   await session.close();
   await driver.close();
 
   // Returns the result.
-  return result;
+  return { queryResult: result, executionTime: endTime - startTime };
 };
 
 module.exports = runQuery;
