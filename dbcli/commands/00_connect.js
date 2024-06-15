765d3e7b70d819421e522ff1fa7b5b6a6178c487
ebbmango <ebbmango@tuta.io>
2024-06-15 18:53:27 +0200

    listens for active database connections and helpfully instruct the user in case of connection failure

--- dbcli/commands/00_connect.js
+++ dbcli/commands/00_connect.js
@@ -2,6 +2,7 @@ const os = require("os");
 const path = require("path");
 const { spawn } = require("child_process");
 const updateSpinner = require("../functions/updateSpinner");
+const isReachable = require("is-reachable");
 
 const command = {
   command: "connect",
@@ -34,13 +35,17 @@ const command = {
     let spinner = ora("Setting up database connection").start(); // Initializes the spinner
     updateSpinner(spinner);
 
+    const checkDatabaseConnection = async () => {
+      const reachable = await isReachable("localhost:7474");
+      return reachable;
+    };
+
     // Listens for stdout data (and formats it adequately)
     dockerProcess.stdout.on("data", (data) => {
       const output = data.toString("utf-8");
       const infoParts = output.split("INFO");
 
       if (infoParts.length > 1) {
-        // console.log(infoParts[1].trim());
         if (infoParts[1].includes("Remote interface available at")) {
           spinner.clear(); // Clears the spinner
           spinner.succeed("Database is ready to accept connections.");
@@ -53,24 +58,54 @@ const command = {
       }
     });
 
+    const checkInterval = setInterval(async () => {
+      const isConnected = await checkDatabaseConnection();
+      if (isConnected) {
+        clearInterval(checkInterval);
+        spinner.clear(); // Clears the spinner
+        spinner.succeed("Database is ready to accept connections.");
+        spinner.warn(
+          "Keep this terminal open to prevent disconnection from the database. Open a separate instance for running commands.\n"
+        );
+      }
+    }, 2500); // Check every 2.5 seconds
+
     // Listens for stderr data
     dockerProcess.stderr.on("data", (data) => {
+      clearInterval(checkInterval);
       spinner?.clear(); // Clears the spinner
-      spinner?.fail("Database connection failed.\n");
-      spinner = null;
 
-      console.error(data.toString("utf-8"));
+      const output = data.toString("utf-8");
+
+      // If the failure is due to the port already being allocated:
+      if (output.includes("port is already allocated")) {
+        spinner.clear();
+        spinner.fail(
+          "Database connection failed: Port 7687 is already allocated."
+        );
+        spinner.warn(
+          "Double-check whether no other database instance is already online.\n"
+        );
+      } else {
+        spinner.clear();
+        spinner.fail("Database connection failed.");
+        spinner.warn(
+          "Make sure to have Docker Desktop running in the background.\n"
+        );
+      }
+
+      spinner = null;
     });
 
     // Listens for errors
     dockerProcess.on("error", (error) => {
-      // spinner.clear(); // Clears the spinner
       console.error(`Error spawning Docker process: ${error}`);
     });
 
     // Listens for process exit
     dockerProcess.on("close", (code) => {
       console.log(`Docker process exited with code ${code}`);
+      process.exit();
     });
   },
 };
